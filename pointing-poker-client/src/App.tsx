41cec957e1cdce2ed8033ac01bf62461c038de53
Nickita Lashch <holbootla@gmail.com>
2021-09-03 10:13:12 +0300

    feat: add bootstrap-react and sass

--- pointing-poker-client/src/App.tsx
+++ pointing-poker-client/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from 'react-bootstrap';
+
 function App(): JSX.Element {
-  return <div>TEXT</div>;
+  return (
+    <div>
+      <Button variant="primary" className="m-1">
+        Example button
+      </Button>
+    </div>
+  );
 }
 
 export default App;

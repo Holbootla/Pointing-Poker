3ef9498936a93aa5b16333d67f8656296dc8c6f7
SK-website <vnali@tutanota.com>
2021-09-06 15:50:47 +0300

    feat: add issueGame component

--- pointing-poker-client/src/app/components/scram/issue-lobby/issue-lobby.tsx
+++ pointing-poker-client/src/app/components/scram/issue-lobby/issue-lobby.tsx
@@ -4,16 +4,14 @@ function Issue(): JSX.Element {
 
   return (
     <li className="item item-issue">
-			<div className="issue-data">
-				<p className="issue-status">Current</p>
-				<p className="issue-name">Issue Title</p>
-				<p className="issue-priority">Priority</p>
-			</div>
-			<div className="issue-actions-wrap">
-			<div className="issue-actions issue-edit" />
-			<div className="issue-actions issue-delete" />
-			</div>
-
+      <div className="issue-data">
+        <p className="issue-name">Issue Title</p>
+        <p className="issue-priority">Priority</p>
+      </div>
+      <div className="issue-icons-wrap">
+      <div className="issue-icon issue-edit" />
+      <div className="issue-icon issue-delete" />
+      </div>
     </li>
   );
 }

3903f255a7cb84e15dfffb06037dc77f2b635551
Comingsoon <comingsoon000@ya.ru>
2021-09-10 05:54:24 +0300

    feat: attach issue popup

--- pointing-poker-client/src/app/components/scrum/new-issue/new-issue.tsx
+++ pointing-poker-client/src/app/components/scrum/new-issue/new-issue.tsx
@@ -1,10 +1,23 @@
+import { useAppDispatch } from '../../../redux/hooks';
+import { showIssuePopupAction } from '../../../redux/reducers/issue-reducer';
+import IssuePopup from '../../shared/issue-popup/Issue-popup';
 import './new-issue.scss';
 
 function NewIssue(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const showIssuePopup = () => dispatch(showIssuePopupAction());
+  
   return (
     <li className="item item-issue">
       <p className="issue-name new-issue-name">create new issue</p>
-      <div className="issue-add" />
+      <div
+        className="issue-add"
+        role="button"
+        tabIndex={0}
+        onKeyPress={showIssuePopup}
+        onClick={showIssuePopup}/>
+      <IssuePopup />
     </li>
   );
 }

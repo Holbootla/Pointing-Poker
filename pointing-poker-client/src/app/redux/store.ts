765efc2ad128b4daf712c9b08dd3e88014983ab8
Comingsoon <comingsoon000@ya.ru>
2021-09-10 04:47:46 +0300

    feat: add kickPopupSlice reducer

--- pointing-poker-client/src/app/redux/store.ts
+++ pointing-poker-client/src/app/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { authPopupSlice } from './reducers/auth-reducer';
 import { exampleSlice } from './reducers/example-reducer';
+import { kickPopupSlice } from './reducers/kick-reducer';
 
 export const store = configureStore({
   reducer: {
     example: exampleSlice.reducer,
     authPopup: authPopupSlice.reducer,
+    kickPopup: kickPopupSlice.reducer,
   },
 });
 

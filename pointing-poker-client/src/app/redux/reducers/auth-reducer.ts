4d3b38dbe43680a45663429af8629042771975bb
Nickita Lashch <holbootla@gmail.com>
2021-09-11 17:26:14 +0300

    feat: add start page logic for creating user and game-id

--- pointing-poker-client/src/app/redux/reducers/auth-reducer.ts
+++ pointing-poker-client/src/app/redux/reducers/auth-reducer.ts
@@ -3,10 +3,28 @@ import type { RootState } from '../store';
 
 interface AuthPopupState {
   authPopupVisible: boolean;
+  gameID: number | '';
+  newGame: boolean;
+  user: {
+    firstName: string | undefined;
+    lastName: string | undefined;
+    jobPosition: string | undefined;
+    isAdmin: boolean;
+    role: 'observer' | 'player';
+  };
 }
 
 const initialState: AuthPopupState = {
   authPopupVisible: false,
+  gameID: '',
+  newGame: false,
+  user: {
+    firstName: undefined,
+    lastName: undefined,
+    jobPosition: undefined,
+    isAdmin: false,
+    role: 'player',
+  },
 };
 
 export const authPopupSlice = createSlice({
@@ -18,12 +36,49 @@ export const authPopupSlice = createSlice({
     },
     closeAuthPopupAction: (state) => {
       state.authPopupVisible = false;
+      state.gameID = '';
+      state.user = {
+        firstName: undefined,
+        lastName: undefined,
+        jobPosition: undefined,
+        isAdmin: false,
+        role: 'player',
+      };
+    },
+    setGameIDAction: (state, action) => {
+      state.gameID = action.payload;
+    },
+    setNewGame: (state, action) => {
+      state.newGame = action.payload;
+    },
+    setFirstNameAction: (state, action) => {
+      state.user.firstName = action.payload;
+    },
+    setLastNameAction: (state, action) => {
+      state.user.lastName = action.payload;
+    },
+    setJobPositionAction: (state, action) => {
+      state.user.jobPosition = action.payload;
+    },
+    setIsAdminAction: (state, action) => {
+      state.user.isAdmin = action.payload;
+    },
+    setRoleAction: (state, action) => {
+      state.user.role = action.payload;
     },
   },
 });
 
-export const { showAuthPopupAction, closeAuthPopupAction } =
-  authPopupSlice.actions;
+export const {
+  showAuthPopupAction,
+  closeAuthPopupAction,
+  setGameIDAction,
+  setNewGame,
+  setFirstNameAction,
+  setLastNameAction,
+  setJobPositionAction,
+  setRoleAction,
+} = authPopupSlice.actions;
 
 export const authPopupVisible = (state: RootState): boolean =>
   state.authPopup.authPopupVisible;

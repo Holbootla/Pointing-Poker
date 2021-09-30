a9c082df00ca5092ccaebaff54e01ead480c6c92
Comingsoon <comingsoon000@ya.ru>
2021-09-30 18:56:14 +0300

    feat: add avatar MongoDB support

--- pointing-poker-server/src/socket-reducer.ts
+++ pointing-poker-server/src/socket-reducer.ts
@@ -1,14 +1,12 @@
 import { Socket } from 'socket.io';
 import { io } from './server';
-import { STATE } from './state';
-import fs from 'fs';
-import path from 'path';
 
 import {
   addIssue,
   addRoundInStatistics,
   addUser,
   addVote,
+  avatarUpload,
   changeGameName,
   changeSettings,
   createState,
@@ -70,27 +68,21 @@ export async function handleAction(
   }
 
   if (action.type === 'user_avatar_uploaded') {
-    const photoDir = path.resolve(__dirname, './temp/')
-    if (!fs.existsSync(photoDir)) {
-      fs.mkdirSync(photoDir)
-    }
-    const writer = fs.createWriteStream(path.resolve(photoDir, action.payload.avatar.name), {
-      encoding: 'base64'
-    });
-    writer.write(action.payload.avatar.data);
-    writer.end();
-    STATE[getStateIndex()].users.map((user) => {
-      if (user.id === action.payload.memberId)
-        user.avatar = 'http://localhost:3001/temp/' + action.payload.avatar.name;
-      console.log(user.avatar);
-    });
+    
+    const newStateFromDb = await avatarUpload(
+      gameID,
+      action.payload.memberId,
+      action.payload.avatar.name,
+      action.payload.avatar.data
+    );
+
     io.to(gameID).emit('UPDATE_CLIENT', {
       type: 'members/setMembersAction',
       payload: {
-        members: STATE[getStateIndex()].users,
+        members: newStateFromDb.users,
       },
     });
-    console.log(STATE, STATE[getStateIndex()].users);
+
   };
 
 

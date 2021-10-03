83ade9e8e8ae4074a5b35a868024b429e7d1ac94
Nickita Lashch <holbootla@gmail.com>
2021-10-03 17:16:44 +0300

    fix: default value of vote

--- pointing-poker-client/src/app/redux/reducers/auth-reducer.ts
+++ pointing-poker-client/src/app/redux/reducers/auth-reducer.ts
@@ -47,7 +47,7 @@ export const authPopupSlice = createSlice({
         jobPosition: '',
         isAdmin: false,
         role: 'player',
-        voteResult: '-',
+        voteResult: '',
       };
     },
     setGameIDAction: (state, action) => {

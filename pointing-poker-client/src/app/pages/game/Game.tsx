d66bc068f595e3febb142932016df4851a214087
Comingsoon <comingsoon000@ya.ru>
2021-10-06 23:21:05 +0300

    fix: fix game

--- pointing-poker-client/src/app/pages/game/Game.tsx
+++ pointing-poker-client/src/app/pages/game/Game.tsx
@@ -54,6 +54,7 @@ function Game(): JSX.Element {
   const { isAdmin, role, id } = useAppSelector((store) => store.authPopup.user);
   const { gameID } = useParams<{ gameID: string }>();
   const [showAlert, setShowAlert] = useState(false);
+  const [chosenCard, setChosenCard] = useState('');
   const thisMemberId = id;
   isVotingFinished = cardsAutoTurn && votes.length === playersQuantity;
 
@@ -115,6 +116,7 @@ function Game(): JSX.Element {
   };
 
   const finishRound = () => {
+    setChosenCard('');
     sendToServer('finish_round', { gameID });
   };
 
@@ -127,6 +129,7 @@ function Game(): JSX.Element {
   };
 
   const cardClickHandler = (cardValue: string): void => {
+    setChosenCard(cardValue);
     if (roundStatus === 'in progress') {
       sendToServer('set_vote', {
         gameID,
@@ -149,12 +152,20 @@ function Game(): JSX.Element {
     issueId: number | string,
     status: IssueStatus
   ): void => {
-    if (roundStatus === 'awaiting' && status === 'awaiting' && isAdmin) {
+    if (roundStatus === 'awaiting' && status === 'awaiting' && isAdmin && !showRestartControls) {
       const newCurrentIssue = issues.find((issue) => issue.id === issueId);
       sendToServer('set_current_issue', {
         gameID,
         currentIssue: { ...newCurrentIssue, status: 'current' },
       });
+    }
+  };
+
+  const leaveGame = () => {
+    if (isAdmin) {
+      sendToServer('game_canceled_admin', { gameID });
+    } else {
+      sendToServer('game_canceled', { gameID, memberId: thisMemberId });
     }
   };
 
@@ -238,7 +249,16 @@ function Game(): JSX.Element {
                           className="m-1"
                           onClick={() => finishRound()}
                         >
-                          Next&nbsp;issue
+                          Finish&nbsp;round
+                        </Button>
+                      )}
+                      {roundStatus === 'in progress' && (
+                        <Button
+                          variant="primary"
+                          className="m-1"
+                          onClick={() => stopRound()}
+                        >
+                          stop&nbsp;round
                         </Button>
                       )}
                         <Button
@@ -251,6 +271,13 @@ function Game(): JSX.Element {
                     </>
                   )}
                   <Chat size={undefined} />
+                  <Button
+                    variant="outline-danger"
+                    className="m-1"
+                    onClick={() => leaveGame()}
+                  >
+                    Cancel game
+                  </Button>
                 </Col>
             </Row>
             <Row className="mb-5">
@@ -339,9 +366,13 @@ function Game(): JSX.Element {
                 onKeyPress={() => cardClickHandler(cardValue)}
               >
                 {cardValue === 'Break' ? (
-                  <CardBreak key="cardBreack" />
+                  <div className={(chosenCard === cardValue) ? 'game__card_chosen' : 'game__card'}>
+                    <CardBreak key="cardBreack" />
+                  </div>
                 ) : (
-                  <CardFace value={cardValue} type={scoreTypeShort} />
+                  <div className={(chosenCard === cardValue) ? 'game__card_chosen' : 'game__card'}>
+                    <CardFace value={cardValue} type={scoreTypeShort} />
+                  </div>
                 )}
               </div>
             </Col>
